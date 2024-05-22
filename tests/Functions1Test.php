15d4755647faecbfb82da6944399a0145f1ed087
ByMorcillo <hutrillaswork@gmail.com>
2024-05-22 17:14:33 +0200

    Pruebas v7

--- tests/Functions1Test.php
+++ tests/Functions1Test.php
@@ -7,26 +7,29 @@ class Functions1Test extends TestCase
 {
     public function testCheckPassword()
     {
-        $this->assertTrue(checkPassword('Abcdefg1'));
-        $this->assertFalse(checkPassword('abc123')); // No contiene mayúsculas
-        $this->assertFalse(checkPassword('ABCDEFGH')); // No contiene minúsculas
-        $this->assertFalse(checkPassword('Abcdefgh')); // No contiene números
-        $this->assertFalse(checkPassword('Abcde1')); // La longitud es menor que 8
+	$funcion = new Functions1();
+        $this->assertTrue($funcion->checkPassword('Abcdefg1'));
+        $this->assertFalse($funcion->checkPassword('abc123')); // No contiene mayúsculas
+        $this->assertFalse($funcion->checkPassword('ABCDEFGH')); // No contiene minúsculas
+        $this->assertFalse($funcion->checkPassword('Abcdefgh')); // No contiene números
+        $this->assertFalse($funcion->checkPassword('Abcde1')); // La longitud es menor que 8
     }
 
     public function testCheckDNI()
     {
-        $this->assertTrue(checkDNI('12345678Z'));
-        $this->assertFalse(checkDNI('1234567Z')); // Longitud incorrecta
-        $this->assertFalse(checkDNI('12345678A')); // Letra incorrecta
-        $this->assertFalse(checkDNI('12345678')); // Falta letra
-        $this->assertFalse(checkDNI('ABC12345X')); // DNI contiene letras en lugar de números
+	$funcion = new Functions1();
+        $this->assertTrue($funcion->checkDNI('12345678Z'));
+        $this->assertFalse($funcion->checkDNI('1234567Z')); // Longitud incorrecta
+        $this->assertFalse($funcion->checkDNI('12345678A')); // Letra incorrecta
+        $this->assertFalse($funcion->checkDNI('12345678')); // Falta letra
+        $this->assertFalse($funcion->checkDNI('ABC12345X')); // DNI contiene letras en lugar de números
     }
 
     public function testCipherPass()
     {
+	$funcion = new Functions1();
         $password = 'TestPassword123';
-        $ciphertext = cipherPass($password);
+        $ciphertext = $funcion->cipherPass($password);
         $this->assertNotEquals($password, $ciphertext); // La contraseña cifrada debe ser diferente a la original
     }
 }
